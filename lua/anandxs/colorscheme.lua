f0d3eb295714055b6569af72b4719a69579c81b0
Anand S <anandsatheeshk@gmail.com>
2023-04-20 21:32:11 +0530

    made background transparent

--- lua/anandxs/colorscheme.lua
+++ lua/anandxs/colorscheme.lua
@@ -5,3 +5,6 @@ if not status_ok then
   vim.notify("colorscheme " .. colorscheme .. " not found!")
   return
 end
+
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
