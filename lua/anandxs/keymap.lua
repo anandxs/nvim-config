7689b785b2bf9c7f43f10dae381b5602b82db9f2
Anand S <anandsatheeshk@gmail.com>
2023-04-22 09:25:47 +0530

    configured telescope basic

--- lua/anandxs/keymap.lua
+++ lua/anandxs/keymap.lua
@@ -62,6 +62,6 @@ keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
 keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
 
 -- Telescope keymaps
-keymap("n", "<leader>ff", "<cmd>Telescope find_files<cr>", opts)
+-- keymap("n", "<leader>ff", "<cmd>Telescope find_files<cr>", opts)
 -- keymap("n", "<leader>ff", "<cmd>lua require'telescope.builtin'.find_files(require('telescope.themes').get_dropdown({ previewer = false }))<cr>", opts)
-keymap("n", "<c-t>", "<cmd>Telescope live_grep<cr>", opts)
+-- keymap("n", "<c-t>", "<cmd>Telescope live_grep<cr>", opts)
--- lua/anandxs/plugins.lua
+++ lua/anandxs/plugins.lua
@@ -70,7 +70,6 @@ return packer.startup(function(use)
   -- Telescope
   use {
       'nvim-telescope/telescope.nvim', tag = '0.1.1',
-      -- or                            , branch = '0.1.x',
       requires = { {'nvim-lua/plenary.nvim'} }
   }
 
